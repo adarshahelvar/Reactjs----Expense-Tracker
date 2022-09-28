2b0de81da99b5e5991b0367f9d7a8f96b886ed09
Adarsha A <adarshaadi1997@gmail.com>
2022-09-28 12:45:00 +0530

    First Hardcode

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+  return (
+    <div>
+        <div>Expense Items</div>
+        <div>Food RS 10</div>
+      <div>March 28 2022</div>
+      <div>
+        <h2>Car Insurence</h2>
+        <div>$5999</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
