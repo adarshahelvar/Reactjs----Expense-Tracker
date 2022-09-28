df3f39cd4fcaa0070f9ea039b9b55c83e205b936
Adarsha A <adarshaadi1997@gmail.com>
2022-09-28 13:50:24 +0530

    Removing some-hardCode

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItems.css';
 
 function ExpenseItem() {
+    const expenseDate= new Date(2021,2,28);
+    const expenseTitle = "car insurence";
+    const expenseAmount = 5900;
   return (
     <div className='expense-item'>
-      <div>March 28 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurence</h2>
-        <div className='expense-item__price'>$5999</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
