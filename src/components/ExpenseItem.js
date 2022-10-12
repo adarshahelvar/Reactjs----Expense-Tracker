404a5a1c2f53a751d90d7c69551d6bcd38c15813
Adarsha A <adarshaadi1997@gmail.com>
2022-10-12 13:26:27 +0530

    Manipulating Title

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>{
+  const [title,setTitle] = useState(props.title);
+  const [amount,setAmount] = useState(props.amount);
+
   const clickHandler =() =>{
-      console.log('clicked Change Title !!!')
+    setTitle('Updated Title')
+    console.log(title)
     }
+
+    const changeExpense =() =>{
+      setAmount('100')
+      console.log('Amount changed')
+      }  
+
   const clickDelete =() =>{
     console.log('clicked Delete!!!')
   }
@@ -15,11 +26,12 @@ const ExpenseItem = (props) =>{
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
       <button onClick={clickDelete}>Delete</button>
+      <button onClick={changeExpense}>Change Expense</button>
     </Card>
   );
 }
