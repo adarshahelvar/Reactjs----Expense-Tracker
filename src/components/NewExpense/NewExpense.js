05875d92f74476b6b8f09bd0f8b9c316ea97a658
Adarsha A <adarshaadi1997@gmail.com>
2022-10-14 08:21:20 +0530

    Form Condition and Adding Cancel Button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense =(props) =>{
+    const [isEditing , setIsEditing] = useState(false);
 
 const saveExpenseDataHandler = (enterdExpenseData) =>{
     const expenseData ={
@@ -10,13 +11,65 @@ const saveExpenseDataHandler = (enterdExpenseData) =>{
         id:Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
 }
 
+const startEditingHandler = () =>{
+    setIsEditing(true);
+}
+
+const stopEditingHandler = () =>{
+    setIsEditing(false);
+}
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}/>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expenses</button> }
+            { isEditing && <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} onCancel={stopEditingHandler}/> }
         </div>
     )
 }
 
 export default NewExpense;
+
+
+
+// import React, { useState } from "react";
+ 
+// import ExpenseForm from "./ExpenseForm";
+// import "./NewExpense.css";
+ 
+// const NewExpense = (props) => {
+//     // we can call a function in new component in  we pass data as a parameter, Can communicate the component to app.
+ 
+//     const [isEditing, setIsEditing] = useState(false);
+ 
+//     const saveExpenseDataHandler = (enteredExpenseData) => {
+//         const expenseData = {
+//             ...enteredExpenseData,
+//             id: Math.random().toString(),
+//         };
+ 
+//         props.onAddExpense(expenseData);
+//         setIsEditing(false);
+//     };
+ 
+//     const startEditingHandler = () => {
+//         setIsEditing(true);
+//     };
+//     const stopEditingHandler = () => {
+//         setIsEditing(false);
+//     };
+//     return ( <div> className = "new-expense" > {!isEditing && ( 
+//         <button onClick = { startEditingHandler } > Add NewButton </button>
+//             )} 
+//             {
+//             isEditing && ( <
+//                 ExpenseForm onSaveExpenseData = { saveExpenseDataHandler }
+//                 onClick = { stopEditingHandler }
+//                 />
+//             )} 
+//             </div>
+//     );
+// };
+ 
+// export default NewExpense;
